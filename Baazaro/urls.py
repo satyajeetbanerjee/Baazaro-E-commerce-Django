50065c42a036ca56e3055a60f6e8544fc8f1c45a
Satyajeet Banerjee <banerjeesatyajeet963@gmail.com>
2025-06-06 02:09:27 +0530

    Normalize CSS line endings to LF

--- Baazaro/urls.py
+++ Baazaro/urls.py
@@ -26,8 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('store/', include(store.urls)),
-]
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
